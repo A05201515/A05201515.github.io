f98be4a822a926f03b592f00a45a33973f555fb3
A05201515 <mav281@txstate.edu>
2026-09-23 16:40:18 -0500

    Improved web map table.

--- javascript/external.js
+++ javascript/external.js
@@ -2,9 +2,16 @@
 // document.getElementById("greeting").textContent = "Hello, welcome " + user_name + "!";
 var webmaps =
 [
-  ["Uber Eats", "https://ubereats.com", "Uber Eats provides users with restaurant options that are within a feasible radius for delivery or pickup. The strength of this feature is its accessibility to the public, whereas its weakness is occasional inaccurate pin placements, and drivers have reported experiencing GPS lag. Uber Eats was designed for public use/everyday consumers, and the user interface is fairly intuitive."],
+  ["Uber Eats", "https://ubereats.com"], ["Uber Eats provides users with restaurant options that are within a feasible radius for delivery or pickup.",
+  "The strength of this feature is its accessibility to the public, whereas its weakness is occasional inaccurate pin placements.",
+  "Drivers have reported experiencing GPS lag.",
+  "Uber Eats was designed for public use/everyday consumers, and the user interface is fairly intuitive."],
    [
-     "ETA Transit", "https://etatransit.com/", "ETA Transit helps public transportation systems be more efficient and reliable for commuters. The strengths of this app are its ability to provide readily available arrival/commute times and delays to passengers, and it allows operators to troubleshoot when there is a problem with a route. The app was designed for passengers, public transportation systems, and the institutions/local governments that provide the transportation system to the public."
+     "ETA Transit", "https://etatransit.com/"],
+     ["ETA Transit helps public transportation systems be more efficient and reliable for commuters.",
+    "The strengths of this app are its ability to provide readily available arrival/commute times and delays to passengers.",
+    "It allows operators to troubleshoot when there is a problem with a route.",
+    "The app was designed for passengers, public transportation systems, and the institutions/local governments that provide the transportation system to the public."
    ]
 ];
 
@@ -21,15 +28,35 @@ function welcome()
 function webmap_table()
 {
   document.write("<table width=100%>");
+
   for (var row=0; row < webmaps.length; row++)
   {
-    document.write("<tr>");
-    for (var column=0; column < webmaps[0].length; column++)
+    if (row % 2 == 0)
     {
-      document.write("<td>" + webmaps[row][column] + "</td>");
+      document.write("<tr>");
+
+      for (var column=0; column < webmaps[row].length; column++)
+      {
+        document.write("<td>" + webmaps[row][column] + "</td>");
+      }
+
+      document.write("</tr>");
     }
-    document.write("</tr>");
+    else
+    {
+      document.write("<tr>");
+      document.write("<td colspan='2'>");
+
+      for (var column=0; column < webmaps[row].length; column++)
+      {
+        document.write(webmaps[row][column] + "<br>");
+      }
+
+      document.write("</td>");
+      document.write("</tr>");
     }
-    document.write("</table>");
-    return "";
+  }
+
+  document.write("</table>");
+  return "";
 }
